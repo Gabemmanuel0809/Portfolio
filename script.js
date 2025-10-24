18ef0d3fdd8e8456dc24468de3802b3b032218ee
Gabriel Emmanuel P. Domondon <gabemmanuel468@gmail.com>
2025-10-24 17:53:54 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var cmt = document.getElementById("comment");
+let button = document.getElementById("btn");
+let list = JSON.parse(localStorage.getItem("comments")) || [];
+
+button.addEventListener("click", function(event) {
+   event.preventDefault();
+    if (cmt.value === "") {
+        alert("Please add comment first before submitting");
+    } else {
+        list.push(cmt.value);
+        window.location.href = "message.html";
+        localStorage.setItem("comments", JSON.stringify(list));
+        cmt.value = "";
+    }
+});
